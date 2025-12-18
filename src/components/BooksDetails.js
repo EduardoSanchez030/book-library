d41e356fdd8d4b75a3cb1ead50804148df8d00f2
Martin <eduardosanchez030@gmail.com>
2025-12-18 13:49:16 -0600

    End of section 12

---
+++ src/components/BooksDetails.js
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import { BooksContext } from "../context/BooksContext";
+import { useParams } from "react-router-dom";
+
+export default function BookDetails() {
+  const { bookId } = useParams();
+  const books = useContext(BooksContext);
+
+  const book = books.find((b) => b.id === parseInt(bookId));
+
+  if (books.lenght === 0) {
+    return <div>...Loading</div>;
+  }
+
+  if (!book) return <div>Book nor found</div>;
+
+  return (
+    <div>
+      <h1>{book.title}</h1>
+      <h2>by {book.author}</h2>
+      <p>{book.description}</p>
+    </div>
+  );
+}
---
+++ src/components/SecretBooks.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function SecretBooks() {
+  return (
+    <div>
+      <ul>
+        <li>secret book 1</li>
+        <li>secret book 2</li>
+        <li>secret book 3</li>
+      </ul>
+    </div>
+  );
+}
